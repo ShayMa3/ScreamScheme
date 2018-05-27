986308a23ddaf7d25aa7f6b8858c329ff938271e
Evan Chee <evancheese1@gmail.com>
2018-05-27 07:48:38 -0700

    Pleb class commit

---
+++ app/src/main/java/com/example/sma51/screamscheme/Pleb.java
@@ -0,0 +1,29 @@
+package com.example.sma51.screamscheme;
+
+public class Pleb {
+	private int hp, dmg;
+
+	public Pleb () {
+		this.hp = 25;
+		this.dmg = 2;
+	}
+
+	public int getBasicAttackCooldown () {
+		return 5;
+	}
+
+	public void takeDamage(int dmgDealt) {
+		if(hp >= 0) {
+			if(level == 1) {
+				hp-=dmgDealt; //take the hp from the boss in the host activity and decide whether or not the boss is dead
+			}
+		}
+		else {
+			hp = 0; //calculate that 0 hp = dead in the actual activity
+		}
+	}
+
+	public int getDmg () {
+		return dmg; //the host activity will subtract this from the character's hp
+	}
+}

88e0ba10649a4dff2c62477d4be1fccd336f2ec0
Evan Chee <evancheese1@gmail.com>
2018-05-27 08:36:41 -0700

    quick commit to save progress on gameActivity

--- app/src/main/java/com/example/sma51/screamscheme/GameActivity.java
+++ app/src/main/java/com/example/sma51/screamscheme/GameActivity.java
@@ -7,26 +7,34 @@ import android.widget.TextView;
 
 public class GameActivity extends AppCompatActivity {
 
-    private TextView textHeroName, textHeroHp, textBossName, textBossHp;
-    String name;
+    private TextView textHeroName, textHeroHp, textBossName, textBossHp, textPoke, textSlap, textPunch, textStab, textPew;
+    private Boss boss;
+    private Hero hero;
+    private Pleb pleb;
+    private Damage poke, slap, punch, stab, pew;
+    private String name;
+    private long startTime, waitTime;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game);
 
-        Hero hero = new Hero("name", 1);
+        hero = new Hero("name");
+        boss = new Boss("Lv. 1 Boss", 50, 5);
+        pleb = new Pleb();
+        poke = new Damage("poke", 1, 0);
+        slap = new Damage("slap", 3, 2);
+        punch = new Damage("punch", 5, 5);
+        stab = new Damage("stab", 8, 10);
+        pew = new Damage("pew", 10, 15);
 
         Intent i = getIntent();
         name = i.getStringExtra("name");
 
         wireWidgets();
-        displayHeroName();
-        
-    }
-
-    private void displayHeroName() {
-        textHeroName.setText(name);
+        displayNames();
+        displayCooldownBar();
     }
 
     private void wireWidgets() {
@@ -34,5 +42,46 @@ public class GameActivity extends AppCompatActivity {
         textHeroHp = (TextView) findViewById(R.id.text_hero_hp);
         textBossName = (TextView) findViewById(R.id.text_boss_name);
         textBossHp = (TextView) findViewById(R.id.text_boss_hp);
+        textPoke = (TextView) findViewById(R.id.text_poke);
+        textSlap = (TextView) findViewById(R.id.text_slap);
+        textPunch = (TextView) findViewById(R.id.text_punch);
+        textStab = (TextView) findViewById(R.id.text_stab);
+        textPew = (TextView) findViewById(R.id.text_pew);
+    }
+    
+    private void displayNames() {
+        textHeroName.setText(name);
+        textBossName.setText(boss.getName());
+    }
+    
+    private void displayCooldownBar() {
+        textPoke.setText("Poke - 0");
+        textSlap.setText("Slap - 0");
+        textPunch.setText("Punch - 0");
+        textStab.setText("Stab - 0");
+        textPew.setText("Pew - 0");
+    }
+    
+    private void updateCooldownBar(Damage dmg) {
+        if(dmg.getName().equals("slap")) {
+            textPoke.setText("Slap - 5");
+            wait(5000);
+            textPoke.setText("Slap - 0");
+        }
+        else if(dmg.getName().equals("punch")) {
+            textPoke.setText("Punch - 5");
+            wait(5000);
+            textPoke.setText("Punch - 0");
+        }
+        else if(dmg.getName().equals("stab")) {
+            textPoke.setText("Stab - 5");
+            wait(5000);
+            textPoke.setText("Stab - 0");
+        }
+        else if(dmg.getName().equals("pew")) {
+            textPoke.setText("Pew - 5");
+            wait(5000);
+            textPoke.setText("Pew - 0");
+        }
     }
 }
